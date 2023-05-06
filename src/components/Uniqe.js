7057ecc42d757c3b0c8de2d163812117e0f7f93f
RB <82284311+Roman123-game@users.noreply.github.com>
2023-05-06 10:54:57 +0300

    update

--- src/components/Uniqe.js
+++ src/components/Uniqe.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "./Uniqe.css";
 
-// Define card types and their attributes
 const CARD_TYPES = {
-  FIRE: { name: "Fire", damage: 3 },
-  WATER: { name: "Water", damage: 2 },
-  EARTH: { name: "Earth", damage: 1 },
-  WIND: { name: "Wind", damage: 0 },
+  fire: {
+    name: "Fire",
+    damage:4
+  },
+  water: {
+    name: "Water",
+    damage: 3
+  },
+  earth: {
+    name: "Earth",
+    damage: 2
+  },
+  wind: {
+    name: "Wind",
+    damage: 1
+  }
 };
 
 function Uniqe() {
-  // State for the player's and opponent's decks
-  const [playerDeck, setPlayerDeck] = useState([]);
-  const [opponentDeck, setOpponentDeck] = useState([]);
-
-  // State for the player's and opponent's hands
-  const [playerHand, setPlayerHand] = useState([]);
-  const [opponentHand, setOpponentHand] = useState([]);
-
-  // State for the player's and opponent's health
   const [playerHealth, setPlayerHealth] = useState(20);
   const [opponentHealth, setOpponentHealth] = useState(20);
+  const [playerDeck, setPlayerDeck] = useState([]);
+  const [opponentDeck, setOpponentDeck] = useState([]);
+  const [playerHand, setPlayerHand] = useState([]);
+  const [opponentHand, setOpponentHand] = useState([]);
 
   // Shuffle an array of cards
   const shuffleCards = (cards) => {
@@ -31,28 +37,43 @@ function Uniqe() {
     return cards;
   };
 
-  // Draw a card from a deck and add it to a hand
+  // Draw a card from the deck and add it to the hand
   const drawCard = (deck, hand) => {
     if (deck.length > 0) {
-      const card = deck.pop();
+      const card = deck[0];
+      deck.splice(0, 1);
       hand.push(card);
-      return true;
-    } else {
-      return false;
     }
   };
 
   // Play a card from the player's hand
-  const playCard = (cardIndex) => {
-    const card = playerHand[cardIndex];
+  const playCard = (index) => {
+    // Remove the card from the player's hand
     const newPlayerHand = [...playerHand];
-    newPlayerHand.splice(cardIndex, 1)
-
+    const card = newPlayerHand.splice(index, 1)[0];
+    setPlayerHand(newPlayerHand);
 
     // Deal damage to the opponent based on the card type
     const damage = CARD_TYPES[card.type].damage;
     setOpponentHealth(opponentHealth - damage);
-  }
+
+    // Opponent takes turn
+    opponentTurn();
+  };
+
+  // Opponent plays a random card from their hand
+  const opponentTurn = () => {
+    const index = Math.floor(Math.random() * opponentHand.length);
+    const card = opponentHand[index];
+    const newOpponentHand = [...opponentHand];
+    newOpponentHand.splice(index, 1);
+    setOpponentHand(newOpponentHand);
+
+    // Deal damage to the player based on the card type
+    const damage = CARD_TYPES[card.type].damage;
+    setPlayerHealth(playerHealth - damage);
+  };
+
   // Check if the game is over (either player's health is 0)
   useEffect(() => {
     if (playerHealth <= 0) {
@@ -71,55 +92,54 @@ function Uniqe() {
         deck.push({ type });
       }
     }
-    // Shuffle the deck and split it between the player and opponent
+
     const shuffledDeck = shuffleCards(deck);
     const playerDeck = shuffledDeck.slice(0, 20);
     const opponentDeck = shuffledDeck.slice(20, 40);
     setPlayerDeck(playerDeck);
     setOpponentDeck(opponentDeck);
 
-    // Draw 3 cards for the player and 2 cards for the opponent
-    const newPlayerHand = [];
-    const newOpponentHand = [];
-    for (let i = 0; i < 3; i++) {
-      drawCard(playerDeck, newPlayerHand);
+    // Draw 5 cards for the player and opponent
+    const playerHand = [];
+    const opponentHand = [];
+    for (let i = 0; i < 5; i++) {
+      drawCard(playerDeck, playerHand);
+      drawCard(opponentDeck, opponentHand);
     }
-    for (let i = 0; i < 2; i++) {
-      drawCard(opponentDeck, newOpponentHand);
-    }
-    setPlayerHand(newPlayerHand);
-    setOpponentHand(newOpponentHand);
+    setPlayerHand(playerHand);
+    setOpponentHand(opponentHand);
   }, []);
 
   return (
     <div className="main">
-      <h1>Elemental Clash</h1>
-      <h5>Rules: In "Elemental Clash", players take turns playing cards from their hand to deal damage to each other. 
-        Each card has a type (fire, water, earth, or wind) and a corresponding damage value. The game ends when one player's health is reduced to 0.</h5>
-      <div className="player">
-        <h2>Player</h2>
-        <p>Health: {playerHealth}</p>
-        <div className="hand">
-          {playerHand.map((card, index) => (
-            <div
-              key={index}
-              className="card"
-              onClick={() => playCard(index)}
-            >
-              {CARD_TYPES[card.type].name}
-            </div>
-          ))}
-        </div>
+      <div className="health">
+        <div>Player Health: {playerHealth}</div>
+        <div>Opponent Health: {opponentHealth}</div>
       </div>
-      <div className="opponent">
-        <h2>Opponent</h2>
-        <p>Health: {opponentHealth}</p>
-        <div className="hand">
-          {opponentHand.map((card, index) => (
-            <div key={index} className="card">
-              {CARD_TYPES[card.type].name}
-            </div>
-          ))}
+      <div className="hands">
+        <div className="player-hand">
+          <h2>Your Hand:</h2>
+          <div>
+            {playerHand.map((card, index) => (
+              <div
+                key={index}
+                className="card"
+                onClick={() => playCard(index)}
+              >
+                {CARD_TYPES[card.type].name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="opponent-hand">
+          <h2>Opponent's Hand:</h2>
+          <div>
+            {opponentHand.map((card, index) => (
+              <div key={index} className="card">
+                {CARD_TYPES[card.type].name}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
